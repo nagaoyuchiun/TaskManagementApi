cbe49496eab342ee63bfd691b920ad921fc73e4d
YuCheng <yucheng@project-studio.cc>
2023-07-18 20:26:53 +0800

    加入專案檔案。

---
+++ TaskManagementApi/Models/JWTConfig.cs
@@ -0,0 +1,23 @@
+﻿namespace TaskManagementApi.Models
+{
+    public class JWTConfig
+    {
+        /// <summary>
+        /// 發行者
+        /// </summary>
+        /// <value></value>
+        public string Issuer { set; get; }
+
+        /// <summary>
+        /// 加密金鑰
+        /// </summary>
+        /// <value></value>
+        public string SignKey { set; get; }
+
+        /// <summary>
+        /// 設置Token存活多久(分鐘)
+        /// </summary>
+        /// <value></value>
+        public int ExpireDateTime { set; get; }
+    }
+}
